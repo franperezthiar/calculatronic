aa16315140452cd999e8cfc46101ba01c0d30b27
fran <franperezthiar@gmail.com>
2023-11-28 10:38:48 +0100

    inicio

--- src/test/java/calculatronicTest.java
+++ src/test/java/calculatronicTest.java
@@ -5,12 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class calculatronicTest {
 
     @Test
-    void sumar() {
-        Integer num1 = 3;
-        Integer num2 = 2;
+    Integer sumar(Integer num1, Integer num2) {
+        num1 = 3;
+        num2 = 2;
         Integer esperado = 5;
         Integer r = sumar(num1,num2);
         assertEquals(esperado,r);
+        return esperado;
     }
 
     @Test
